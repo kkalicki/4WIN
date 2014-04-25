f6c629ffd3d7cc6990b38096fff83bbfae008dc8
Max <Max@maximilians-mbp.fritz.box>
2014-04-25 12:49:12 +0200

    allgemein Funktionalitaeten, die implementiert werden sollen

--- h/Stein.h
+++ h/Stein.h
@@ -12,6 +12,21 @@ class Stein {
 public:
 	Stein();
 	virtual ~Stein();
+
+	/*falls notwendig:
+		Konstruktoren:
+		Kopierkonstruktor
+		Zuweisungskonstruktor
+		Destruktor
+
+		shiftOperator ueberladen (<<) fuer die Ausgabe
+		toString() implementieren
+
+		fuer toString..
+		1) Objekt von ostringstream erzeugen
+		2) alles fuer den String in das Objekt rein shiften
+		3) return ostringstreamObjekt.str
+		/*
 };
 
 #endif /* STEIN_H_ */
