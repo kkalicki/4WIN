912b4c90f972749f0608819386d75fe4cf9fa0cc
kkalicki <alviso@me.com>
2014-05-04 15:01:17 +0200

    Implementierung Stein begonnen #2

--- h/Stein.h
+++ h/Stein.h
@@ -7,26 +7,49 @@
 
 #ifndef STEIN_H_
 #define STEIN_H_
+#include <string>
+
+using namespace std;
 
 class Stein {
 public:
-	Stein();
+	Stein(int farbe);
+
 	virtual ~Stein();
+
+	string toString();
+
+	short int getFarbe() const {
+		return farbe;
+	}
+
+	void setFarbe(short int farbe) {
+		this->farbe = farbe;
+	}
 
 	/*falls notwendig:
 		Konstruktoren:
 		Kopierkonstruktor
 		Zuweisungskonstruktor
 		Destruktor
-
 		shiftOperator ueberladen (<<) fuer die Ausgabe
 		toString() implementieren
-
 		fuer toString..
 		1) Objekt von ostringstream erzeugen
 		2) alles fuer den String in das Objekt rein shiften
 		3) return ostringstreamObjekt.str
 		*/
+
+	//noch Auslagern
+	const static unsigned short int ROT = 1;
+	const static unsigned short int GELB = 0;
+
+private:
+
+	short int farbe;
 };
+
+
+
 
 #endif /* STEIN_H_ */
