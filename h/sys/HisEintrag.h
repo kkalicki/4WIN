b234f8f241ddd20d526566e02023c44f4e7c23c7
fuckingbobb <max@maxs-Mac-Pro.local>
2014-07-04 16:34:19 +0200

    Kommentare HisEintrag, Historie

--- h/sys/HisEintrag.h
+++ h/sys/HisEintrag.h
@@ -3,41 +3,143 @@
 #include "../h/sys/Spieler.h"
 #include <ctime>
 #include <string>
-
+/**
+ * @brief The HisEintrag class
+ * Klasse, die einen HistorieEintrag im System repraesentiert.
+ */
 class HisEintrag{
 public:
+    /**
+     * HisEintrag   Konstruktor
+     */
     HisEintrag();
+    /**
+     * @HisEintrag  Konstruktor
+     * @param spieler Spieler, der diesem Eintrag zugeordnet werden soll
+     * @param zeile   Zeile in die geworfen wurde
+     * @param spalte  Spalte in die geworfen wurde
+     * @param runde   Aktuelle Runde
+     */
     HisEintrag(Spieler* spieler, unsigned short zeile=0, unsigned short spalte=0, unsigned short runde=0);
     ~HisEintrag();
 
+    /**
+     * getSpieler Getter fuer Spieler
+     * @return Spieler
+     */
     Spieler* getSpieler() const;
+
+    /**
+     * setSpieler Setter fuer Spieler
+     * @param value Wert
+     */
     void setSpieler(Spieler* value);
 
+    /**
+     * getZeile Getter fuer Zeile
+     * @return Zeile
+     */
     unsigned short getZeile() const;
+
+    /**
+     * setZeile Setter fuer Zeile
+     * @param value Wert
+     */
     void setZeile(unsigned short value);
 
+    /**
+     * getSpalte Getter fuer Spalte
+     * @return Spalte
+     */
     unsigned short getSpalte() const;
+
+    /**
+     * setSpalte    Setter fuer Spalte
+     * @param value wert
+     */
     void setSpalte(unsigned short value);
 
+    /**
+     * getTimestamp Getter fuer Timestamp
+     * @return Timestamp
+     */
     time_t getTimestamp() const;
+
+    /**
+     * setTimestamp Setter fuer Timestamp
+     * @param value Wert
+     */
     void setTimestamp(const time_t &value);
 
+    /**
+     * zeitStempel Getter fuer den Zeitstempel als String
+     * @return Timestamp als String
+     */
     string zeitStempel();
 
+    /**
+    * getRunde Getter fuer Runde
+    * @return Runde
+    */
     unsigned short getRunde() const;
+
+    /**
+     * setRunde Setter fuer Runde
+     * @param value Wert
+     */
     void setRunde(unsigned short value);
 
+    /**
+     * fromCsvString        Funktion, um das Objekt von einem CSV-String zu initialisieren
+     * @param csvString     CSV-String
+     */
     void fromCsvString(string csvString);
+
+    /**
+     * toString Funktion, um das Objekt als String zurueckzugeben
+     * @return Objekt als String
+     */
     string toString();
 
+    /**
+     * @brief operator <<  Funktion, zum ueberladen des << Operators
+     * @param out          OstreamObjekt
+     * @param object       HisEintragObjekt
+     * @return             ostream mit HisEintrag
+     */
     friend ostream& operator<<(ostream& out, HisEintrag& object);
+
+    /**
+     * @brief operator >>   Funktion, zum ueberladen des >> Operators
+     * @param in            IstreamObjekt
+     * @param object        HisEintragObjekt
+     * @return              Istream mit HisEintrag
+     */
     friend istream& operator>>(istream& in, HisEintrag &object );
 
 private:
+    /**
+     * spieler Spieler fuer den Eintrag
+     */
     Spieler* spieler;
+    /**
+     * zeile Zeile in die geworfen wurde
+     */
     unsigned short zeile;
+
+    /**
+     * spalte Spalte in die geworfen wurde
+     */
     unsigned short spalte;
+
+    /**
+     * runde Aktuelle Runde
+     */
     unsigned short runde;
+
+    /**
+     * timestamp Timestamp
+     */
     time_t timestamp;
 
 };
--- h/sys/Historie.h
+++ h/sys/Historie.h
@@ -6,25 +6,105 @@
 
 using namespace std;
 
+/**
+ * The Historie class
+ * Klasse, die eine Historie im System repraesentiert. In der Historie wird der gesamte Spielverlauf gespeichert
+ */
 class Historie{
 public:
+    /**
+     * Historie Konstruktor
+     */
     Historie();
+
+    /**
+     * Historie Kopierkonstruktor
+     * @param rhs Objekt, welches kopiert werden soll
+     */
     Historie(const Historie& rhs);
     ~Historie();
+
+    /**
+     * operator = Zuweisungskonstruktor
+     * @param rhs   Objekt, welches zugewiesen werden soll
+     * @return      zugewiesenes Objekt
+     */
     Historie& operator= (Historie const& rhs);
+
+    /**
+     * hinzufuegenEintrag Funktion, um einen Eintrag in die Historie einzutragen
+     * @param hisEintrag Eintrag, welches hinzugefuegt werden soll
+     */
     void hinzufuegenEintrag(const HisEintrag &hisEintrag);
+
+    /**
+     * loeschenEintrag Funktion, um einen Historieeintrag an einem bestimmten Index zu löschen
+     * @param index Index der geloescht werden soll
+     */
     void loeschenEintrag(int index);
+
+    /**
+     * loeschenEintrag Funktion, um einen HistorieEintrag zu loeschen
+     * @param hisEintrag HisEintrag der geloescht werden soll
+     * @return index der geloescht wurde
+     */
     int  loeschenEintrag(HisEintrag hisEintrag);
+
+    /**
+     * loeschenHistorie Funktion, um die gesamte Historie zu loeschen
+     */
     void loeschenHistorie();
+
+    /**
+     * getEintragAt Funktion, um einen Eintrag zurueckzugeben
+     * @param index Index, welcher Eintrag zurueckgegeben werden soll
+     * @return HisEintrag
+     */
     HisEintrag*  getEintragAt(int index);
+
+    /**
+     * getLetztenEintrag Funktion, um den letzten Eintrag in der Historie zurueckzugeben
+     * @return HisEintrag
+     */
     HisEintrag* getLetztenEintrag();
+
+    /**
+     * getEintraegeAb Funktion, um eine Liste aller Eintraege ab einem bestimmten Objekt zurueckzugeben
+     * @param hisEintrag Objekt ab dem die Liste erstellt werden soll
+     * @return Liste mit allen Eintraegen ab einem Objekt
+     */
     vector<HisEintrag> getEintraegeAb(HisEintrag* hisEintrag);
+
+    /**
+     * @brief operator <<   Funktion, zum ueberladen des << Operators
+     * @param out           OstreamObjekt
+     * @param object        HistorieObjekt
+     * @return              ostream mit Historie
+     */
     friend ostream& operator<<(ostream& out, Historie& object);
+
+    /**
+     * toString Funktion, um das Objekt als String zurueckzugeben
+     * @return Objekt als String
+     */
     string toString();
+
+    /**
+     * getHisList Funktion, um die komplette Historie als Liste zurueckzugeben
+     * @return Historie
+     */
     vector<HisEintrag> *getHisList() const;
+
+    /**
+     * setHisList   Setter fuer HisListe
+     * @param value
+     */
     void setHisList(vector<HisEintrag> *value);
 
 private:
+    /**
+     * hisList Vector in dem die HistorienEintraege gespeichert werden
+     */
     vector<HisEintrag>* hisList;
 };
 
